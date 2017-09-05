ebb2d8d0234b509a2de6236bbd4ab05791d3d540
Pat Downey <pat.downey@gmail.com>
2017-09-05 22:36:42 +0100

    add rpm output to build

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -xe
 
 PACKAGE_MAINTAINER="pat.downey@gmail.com"
 PACKAGE_URL="https://github.com/patdowney/secure-demo-puppet"
-PACKAGE_TYPE="deb"
 
 bundle install --path vendor/bundle --binstubs .bundle/bin
 
@@ -20,17 +19,34 @@ PACKAGE_NAME=${PACKAGE_NAME:-$(basename `pwd`)}
 mkdir -p .output
 rm -rf .output/*
 
+PACKAGE_TYPE="deb"
 bundle exec  fpm -t ${PACKAGE_TYPE} -s dir \
   --prefix /usr/share/${PACKAGE_NAME} \
   --package ".output/${PACKAGE_NAME}_0.1.${VERSION}_all.${PACKAGE_TYPE}" \
   --name "${PACKAGE_NAME}" \
   --version "${VERSION}" \
-  --depends 'puppet' \
   --architecture all \
   --maintainer "$PACKAGE_MAINTAINER" \
   --url $PACKAGE_URL \
   --deb-user root \
   --deb-group root \
+  --verbose \
+  bin/ \
+  manifests/ \
+  $(find modules/ -name files -or -name lib -or -name manifests -or -name templates) \
+  $(find vendor/modules/ -name files -or -name lib -or -name manifests -or -name templates)
+
+PACKAGE_TYPE="rpm"
+bundle exec  fpm -t ${PACKAGE_TYPE} -s dir \
+  --prefix /usr/share/${PACKAGE_NAME} \
+  --package ".output/${PACKAGE_NAME}_0.1.${VERSION}_all.${PACKAGE_TYPE}" \
+  --name "${PACKAGE_NAME}" \
+  --version "${VERSION}" \
+  --architecture all \
+  --maintainer "$PACKAGE_MAINTAINER" \
+  --url $PACKAGE_URL \
+  --rpm-user root \
+  --rpm-group root \
   --verbose \
   bin/ \
   manifests/ \
