12f63428e9c8bb1c75abf007f3ccda45429cb9da
Pat Downey <pat.downey@gmail.com>
2017-09-13 21:25:56 +0100

    add header

--- modules/base/manifests/repos.pp
+++ modules/base/manifests/repos.pp
@@ -1,3 +1,21 @@
+# Class: base
+# ===========================
+#
+# Parameters
+# ----------
+#
+# Variables
+# --------
+#
+# Examples
+# --------
+#
+# Authors
+# -------
+#
+# Copyright
+# ---------
+#
 class base::repos(
   $repos = []
 ) {
